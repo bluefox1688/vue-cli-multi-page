2e151e41bdaa39e2d85aeffdb263a2fda621ada9
蓝狐 <bluefox1688>
2016-08-26 22:03:49 +0800

    增加components组件的干净模板
    增加干净模板，复制过来直接可以使用。

---
+++ tpl/components_tpl/Hello.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+  	tpl--{{msg}}
+  </div>
+</template>
+
+<script>
+
+import Lib from 'assets/Lib.js'
+
+module.exports = {
+	components:{
+		
+	},
+  data: function () {
+    return {
+      msg: 'Hello，这是组件的模板'
+    }
+  },
+  ready(){
+  	
+  },
+  methods:{
+  	
+  },
+  props:{
+  	
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
