9aa5ab29485cb4730d18abafed8b9a8e9f2bec0d
蓝狐 <bluefox1688>
2017-05-03 17:37:48 +0800

    修复axios的timeout错误问题

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -39,7 +39,7 @@ var Rxports = {
 			// `baseURL` 将自动加在 `url` 前面，除非 `url` 是一个绝对 URL。
   			// 它可以通过设置一个 `baseURL` 便于为 axios 实例的方法传递相对 URL
 			baseURL:'http://t.lanchenglv.com/tp5demo/index.php/',
-			time: opts.time || 10*1000,
+			timeout: opts.time || 10*1000,
 			responseType: opts.dataType || 'json'
 		}).then(function(res){
 			
